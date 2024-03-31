91b697ab7f73ab7498010cab51ef0b5f11902a5b
parlin <parlin162@gmail.com>
2024-03-31 13:15:00 +0700

    Memanggil  method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa =====================");
+  System.out.println("NIDN : 2210010348");
+  System.out.println("NAMA : Parlin");
+  System.out.println("===================================");
+ }
+}
